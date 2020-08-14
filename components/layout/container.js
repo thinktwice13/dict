9389e9398e7fd0f81ba2962d880670b8acdc4a25
thinktwice13 <marioo.krajacic@gmail.com>
2020-08-14 21:48:52 +0200

    Use styled layout component

--- components/layout/container.js
+++ components/layout/container.js
@@ -1,40 +1,62 @@
 import Head from 'next/head';
 // import styles from '../../styles/Home.module.css';
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Main = styled.div`
+  padding: 5rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Footer = styled.div`
+  width: 100%;
+  height: 100px;
+  border-top: 1px solid #eaeaea;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FooterAnchor = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+`;
 
 // eslint-disable-next-line
 const Conttainer = ({ children }) => {
   return (
-    <div
-    // className={styles.container}
-    >
+    <StyledContainer>
       <Head>
         <title>Dict App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main
-      // className={styles.main}
-      >
-        {children}
-      </main>
+      <Main>{children}</Main>
 
-      <footer
-      // className={styles.footer}
-      >
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+      <Footer>
+        <FooterAnchor
+          href="https://github.com/thinktwice13/dict"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img
-            src="/vercel.svg"
-            alt="Vercel Logo"
-            // className={styles.logo}
-          />
-        </a>
-      </footer>
-    </div>
+          Source code on Github
+        </FooterAnchor>
+      </Footer>
+    </StyledContainer>
   );
 };
 
