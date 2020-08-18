63e5368af82e9af61649add0eb7fd240fb125337
thinktwice13 <marioo.krajacic@gmail.com>
2020-08-18 20:28:51 +0200

    Turn recent list item iinto a liink

--- components/list/recent-item.js
+++ components/list/recent-item.js
@@ -1,15 +1,21 @@
 import { List } from 'antd';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
 
-const RecentListItem = ({ title, description }) => (
-  <List.Item>
-    <List.Item.Meta title={title} description={description} />
-  </List.Item>
+const RecentListItem = ({ id, title, description }) => (
+  <Link href="/phrases/[phraseId]" as={`/phrases/${id}`}>
+    <a>
+      <List.Item>
+        <List.Item.Meta title={title} description={description} />
+      </List.Item>
+    </a>
+  </Link>
 );
 
 RecentListItem.propTypes = {
   title: PropTypes.string.isRequired, // TODO update
   description: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export { RecentListItem };
